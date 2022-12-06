d5995295934ac3b07e72dd97260bc1f084f81527
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-12-06 14:01:07 +0530

    Time: 161 ms (21.48%), Space: 14.2 MB (39.15%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -3,21 +3,15 @@ class Solution:
         
         xCoord, yCoord = 0,0
         
-        def Rmove(xCoord, yCoord):
-            xCoord+=1
-            return xCoord, yCoord
-        def Lmove(xCoord, yCoord ):
-            xCoord-=1
-            return xCoord, yCoord
-        def Umove(xCoord, yCoord):
-            yCoord+=1
-            return xCoord, yCoord
-        def Dmove(xCoord, yCoord):
-            yCoord-=1
-            return xCoord, yCoord
-        
-        mapMoves={'U':Umove, 'D':Dmove, 'R':Rmove, 'L':Lmove}
         for move in moves:
-            xCoord, yCoord = mapMoves[move](xCoord, yCoord)
+            if move == 'R':
+                xCoord+=1
+            elif move == 'L':
+                xCoord-=1
+            elif move == 'U':
+                yCoord+=1
+            else:
+                yCoord-=1
+            
         
         return xCoord==yCoord==0
