146c7f3b8e2931a9039693ad49af33a53f290c3d
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-12-15 23:49:30 +0530

    Time: 120 ms (86.28%), Space: 15.5 MB (76.27%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack=[]
+        n=len(s)
+        indexToDelete=set()
+        for i in range(n):
+            if s[i]=='(':
+                stack.append(i)
+            elif s[i]==')':
+                if not stack:
+                    indexToDelete.add(i)
+                else:
+                    stack.pop()
+        for remainingOpens in stack:
+            indexToDelete.add(remainingOpens)
+        print("indexToDelete=",indexToDelete, stack)
+        result=''
+        for i in range(n):
+            if i in indexToDelete:
+                continue
+            result+=s[i]
+        return result
+        
+'''
+0123456789 10 11 12
+le(e(t(c)o d   e  )
+       ^
+   (6
+   (4
+   (2
+   if char == ')' and stack is empty ---- then delete char index
+   
+'''
