afd816aa4680b1abc844a23e63f0c56dfcd9b46d
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2023-03-11 00:07:02 +0530

    Time: 623 ms (44.53%), Space: 16.3 MB (5.28%) - LeetHub

---
+++ 0457-circular-array-loop/0457-circular-array-loop.py
@@ -0,0 +1,35 @@
+class Solution:
+    
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        n=len(nums)
+        neighbour_list = [0]*n
+        for i in range(n):
+            neighbour_list[i] = normalize_index(nums[i] + i, n)
+        for i in range(n):
+            result = traverse_and_find_cycle(i, nums, neighbour_list, set([]), n)
+            if result:
+                return True
+        return False
+        
+            
+def traverse_and_find_cycle( node, nums, neighbour_list, visitor_set, n, track_seq_pos=0, track_seq_neg=0):
+    if nums[node] >=0 :
+        track_seq_pos+=1
+    else:
+        track_seq_neg+=1
+    if not (track_seq_pos==0 or track_seq_neg==0):
+        return False
+    #print(track_seq_pos, track_seq_neg)
+    if node in visitor_set:
+        return True
+    visitor_set.add(node)
+    next_node = neighbour_list[node]
+    if next_node == node:
+        return False
+    return traverse_and_find_cycle( next_node, nums, neighbour_list, visitor_set, n, track_seq_pos, track_seq_neg)
+    
+    
+def normalize_index(x, array_length):
+    if x>=0:
+        return x % array_length
+    return (array_length - abs(x) % array_length)% array_length
